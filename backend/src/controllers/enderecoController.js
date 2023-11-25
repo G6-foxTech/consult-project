4f3a660e5377e2cc50f10c7b3d68215b17960849
Vini5577 <ugopessoa@gmail.com>
2023-11-25 17:04:52 -0300

    end address configurable

--- backend/src/controllers/enderecoController.js
+++ backend/src/controllers/enderecoController.js
@@ -1,37 +1,58 @@
-const tasksModel = require('../models/tasksModel');
-
-const getAll = async (_req, res) => {
-
-    const tasks = await tasksModel.getAll();
-
-    return res.status(200).json(tasks);
-};
-
-const createTask = async (req, res) => {
-
-    const createdTask = await tasksModel.createTask(req.body);
-
-    return res.status(201).json(createdTask);
-};
-
-const deleteTask = async (req, res) => {
-    const { id } = req.params;
-
-    await tasksModel.deleteTask(id);
-    return res.status(204).json();
-};
-
-const updateTask = async (req, res) => {
-
-    const { id } = req.params;
-
-    await tasksModel.updateTask(id, req.body);
-    return res.status(204).json();
-};
+const enderecoModel = require('../models/enderecoModel');
+const Sequelize = require('sequelize');
 
 module.exports = {
-    getAll,
-    createTask,
-    deleteTask,
-    updateTask
+    async getAll(req, res) {
+        const enderecos = await enderecoModel.findAll();
+
+        return res.status(201).json(enderecos);
+    },
+
+    async getOne(req, res) {
+        try {
+            const endereco = await enderecoModel.findAll({where: {id_endereco: req.params.id_endereco}});
+            return res.json(endereco);
+        } catch(error) {
+            return res.status(404).json({message: 'Endereço não encontrado'});
+        }
+        
+    },
+    
+    async create(req, res) {
+        const { numero, logradouro, bairro, cidade, complemento, cep, uf } = req.body;
+
+        const createEndereco = await enderecoModel.create({ numero, logradouro, bairro, cidade, complemento, cep, uf });
+    
+        return res.status(201).json(createEndereco);
+    },
+
+    async delete(req, res) {
+        const { id_endereco } = req.params;
+        const endereco = await enderecoModel.findByPk(id_endereco);
+
+        if(!endereco) {
+            return res.status(404).json({ message: 'Endereço não encontrado '});
+        }
+
+        await endereco.destroy();
+
+        return res.status(204).send();
+    },
+
+    async update(req, res) {
+        
+        const Op = Sequelize.Op;
+        const { numero, logradouro, bairro, cidade, complemento, cep, uf } = req.body;
+        const id =req.params.id_endereco;
+
+        try {
+            await enderecoModel.update(
+                { numero, logradouro, bairro, cidade, complemento, cep, uf }, 
+                { where: {id_endereco: {[Op.eq]: id}}});
+
+            return res.json({message: 'Endereço atualizado com sucesso'});
+        } catch(err) {
+            return res.json({message: 'Endereço não foi atualizado, pois ele não existe ou foi inserido errado'});
+        }
+    },
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -3,15 +3,29 @@ const express = require('express');
 const router = express.Router();
 
 const enderecoController = require('./controllers/enderecoController');
-const enderecoMiddleware = require('./middlewares/enderecoMiddleware');
 
-router.get('/address', enderecoController.getAll);
-router.post('/address', enderecoMiddleware.validateFieldTitle, enderecoController.createTask);
-router.delete('/address/:id', enderecoController.deleteTask);
-router.put('/address/:id', 
-    enderecoMiddleware.validateFieldTitle, 
-    enderecoMiddleware.validateFieldStatus, 
-    enderecoController.updateTask
-);
+router.get('/', (req, res) => res.send('<h1>Hello World</h1>'));
+
+router.post('/usuario', );
+router.post('/profissional', );
+router.post('/consultorio', );
+router.post('/empresa', );
+router.post('/locacao', );
+router.post('/consultorio-empresa', );
+router.post('/endereco', enderecoController.create);
+
+router.get('/endereco', enderecoController.getAll);
+router.get('/usuario', );
+router.get('/profissional', );
+router.get('/consultorio', );
+router.get('/empresa', );
+router.get('/locacao', );
+router.get('/consultorio-empresa', );
+
+router.get('/endereco/:id_endereco', enderecoController.getOne);
+
+router.put('/endereco/:id_endereco', enderecoController.update);
+
+router.delete('/endereco/:id_endereco', enderecoController.delete);
 
 module.exports = router;
