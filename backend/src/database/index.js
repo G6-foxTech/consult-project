cf240b04453f3558e87cb64d268fe33602cccac1
Vini5577 <ugopessoa@gmail.com>
2023-11-25 22:03:16 -0300

    finish profissional

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,9 +2,13 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
 const endereco = require('../models/enderecoModel');
+const profissional = require('../models/profissionalModel');
 
 const connection = new Sequelize(dbConfig);
 
 endereco.init(connection);
+profissional.init(connection);
+
+profissional.associate(connection.models);
 
 module.exports = connection;
--- backend/src/router.js
+++ backend/src/router.js
@@ -3,29 +3,34 @@ const express = require('express');
 const router = express.Router();
 
 const enderecoController = require('./controllers/enderecoController');
+const profissionalController = require('./controllers/profissionalController');
 
 router.get('/', (req, res) => res.send('<h1>Hello World</h1>'));
 
 router.post('/usuario', );
-router.post('/profissional', );
+router.post('/profissional', profissionalController.create);
 router.post('/consultorio', );
 router.post('/empresa', );
 router.post('/locacao', );
 router.post('/consultorio-empresa', );
 router.post('/endereco', enderecoController.create);
 
-router.get('/endereco', enderecoController.getAll);
 router.get('/usuario', );
-router.get('/profissional', );
+router.get('/profissional', profissionalController.getAll );
 router.get('/consultorio', );
 router.get('/empresa', );
 router.get('/locacao', );
 router.get('/consultorio-empresa', );
+router.get('/endereco', enderecoController.getAll);
 
 router.get('/endereco/:id_endereco', enderecoController.getOne);
+router.get('/profissional/:id_profissional', profissionalController.getOne);
 
+router.put('/profissional/:id_profissional', profissionalController.update);
 router.put('/endereco/:id_endereco', enderecoController.update);
 
+router.delete('/profissional/:id_profissional', profissionalController.delete);
 router.delete('/endereco/:id_endereco', enderecoController.delete);
+
 
 module.exports = router;
