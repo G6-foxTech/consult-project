e015a2650b2a8767855ae0e81a62f5e93efe6360
Hugo Vinícius <ugopessoa@gmail.com>
2023-12-11 09:30:48 -0300

    menu configurable

---
+++ frontend/script/menu.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for(const element of toggle) {
+    element.addEventListener('click', () => {
+        nav.classList.toggle('show')
+    })
+}
+
