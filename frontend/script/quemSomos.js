348a6df11e8d8000130d6de2a6a87ba700d55707
Hugo Vinícius <ugopessoa@gmail.com>
2023-12-13 08:41:01 -0300

    add page quemSomos

---
+++ frontend/script/quemSomos.js
@@ -0,0 +1,21 @@
+const quemSomos = document.querySelector('.quemSomos');
+const missao = document.querySelector('.missao');
+const objetivos = document.querySelector('.objetivo');
+const excelencia = document.querySelector('.excelencia');
+const colaboracao = document.querySelector('.colaboracao');
+const transparencia = document.querySelector('.transparencia');
+const inovacao = document.querySelector('.inovacao');
+const cuidado = document.querySelector('.cuidado');
+
+if(window.innerWidth >= 1200) {
+
+    quemSomos.setAttribute('src', '../img/desktop/Quem somos_.png');
+    missao.setAttribute('src', '../img/desktop/Missão.png');
+    objetivos.setAttribute('src', '../img/desktop/Objetivos.png');
+    excelencia.setAttribute('src', '../img/desktop/Excelência.png');
+    colaboracao.setAttribute('src', '../img/desktop/Colaboração.png');
+    transparencia.setAttribute('src', '../img/desktop/Transparencia.png')
+    inovacao.setAttribute('src', '../img/desktop/Inovação.png');
+    cuidado.setAttribute('src', '../img/desktop/Cuidado.png');
+
+}
